722725b9d561ea1c1c2a220eae6a8cbd3bd0bede
Valdineia Signorelli <valsignorelli@Valdineias-MBP.ec.rr.com>
2018-08-17 21:16:03 -0400

    changes

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheesy Dreamy');
+INSERT INTO burgers (burger_name) VALUES ('Meaty Fantasy');
+INSERT INTO burgers (burger_name) VALUES ('Veggiey Fancy');
+INSERT INTO burgers (burger_name) VALUES ('Fishy Imaginy');
